d48db4b954b73f4a42bc2a956e82f410b901bbb5
Sam <centersam19@gmail.com>
2020-03-01 10:16:09 -0500

    Listen to trigger

--- matrix/main.py
+++ matrix/main.py
@@ -4,7 +4,7 @@ from networktables import NetworkTables
 from Matrix import Matrix
 
 # Connect to the RIO as a client
-NetworkTables.initialize(server='DESKTOP-T5DSCVI.local')
+NetworkTables.initialize(server='10.71.27.197')
 
 # Get an instance of the table
 ledTable = NetworkTables.getTable('PiLED/matrix')
@@ -14,19 +14,20 @@ matrix = Matrix()
 def LED_value_changed(source, key, value, isNew):
     global matrix
     print("==================")
-    print("New file: " + value)
+    filename = ledTable.getString("file", "")
+    print("New file: " + filename)
 
     # Get media type
     effectType = ledTable.getString("type", "off")
     print("Of type: " + effectType)
 
-    result = matrix.display(effectType, value)
+    result = matrix.display(effectType, filename)
     ledTable.putBoolean("result", result)
 
     print("Result:")
     print(result)
 
-ledTable.addEntryListener(LED_value_changed, True, "file")
+ledTable.addEntryListener(LED_value_changed, True, "trigger")
 
 print("Press Ctrl+C to exit")
 while True:
